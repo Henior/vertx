3efe0c2aeaee463adaf2712af2c973a818d0d8fb
mouzhanpeng <mouzhanpeng@corp.tigerjoys.com>
2018-06-08 17:07:29 +0800

    web 服务+ spring容器融合

--- src/main/java/cn/henio/Main.java
+++
@@ -1,1 +0,0 @@
-package cn.henio;import io.vertx.core.DeploymentOptions;import io.vertx.core.Vertx;import io.vertx.core.VertxOptions;/** * created by mouzhanpeng at 2018/2/6 13:39 * * @since jdk 1.9.0 */public class Main {    public static void main(String[] args) {        Vertx.vertx().deployVerticle(FirstVerticle.class.getName());    }}
